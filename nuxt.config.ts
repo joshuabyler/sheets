be8acb560ceac21a828a7284eef100b85b7a31b0
Joshua Byler <bylerj@gmail.com>
2024-09-27 09:31:23 -0400

    fix: got the generate working locally hopefully it works on github

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: 'https://joshuabyler.github.io/sheets/'
+    // baseURL: 'https://joshuabyler.github.io/sheets/'
   },
   devtools: { enabled: true },
   css: ['@/assets/css/tailwind.css'],
   compatibilityDate: '2024-04-03',
   nitro: {
-    prerender: {
-      concurrency: 1000,
-      interval: 1000,
-      crawlLinks: false,
-      failOnError: true
-    },
     preset: "github-pages",
   },
   modules: ['@pinia/nuxt', '@nuxt/ui'],
-  routeRules: {
-    "/": { prerender: true },
-  },
   runtimeConfig: {
     public: {
       GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
